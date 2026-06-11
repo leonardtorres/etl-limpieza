124aa1fa1aa0ced29ce09223585c4dcdf2aa6fb8
Leonard Torres <leonardtorresrodriguez@gmail.com>
2026-06-11 17:25:29 -0500

    Corrige el main

---
+++ src/main.py
@@ -0,0 +1,8 @@
+def sumar(a: int, b: int) -> int:
+    """Suma dos números enteros."""
+    return a + b
+
+
+if __name__ == "__main__":
+    resultado = sumar(5, 3)
+    print(f"La suma de 5 + 3 es: {resultado}")
